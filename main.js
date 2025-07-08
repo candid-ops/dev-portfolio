55295264eb7cafc7e0e9daf24dd75cf753a9444d
candid-ops <abdighafar223@gmail.com>
2025-07-08 13:02:10 -0700

    Completed portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["UI/UX Designer", "JavaScript Engineer", "Front-End Developer", "Back-End Developer", "Full-Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
